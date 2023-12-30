a52e82f8dbc797530f7722a86d02a4aebc9e1f1c
minalsarwar <minalsarwar51@gmail.com>
2023-12-30 09:18:06 +0500

    Final push

--- lib/networking/home_page.dart
+++ lib/networking/home_page.dart
@@ -579,7 +579,7 @@ class HomeContent extends ConsumerWidget {
             borderRadius: BorderRadius.circular(10.0),
           ),
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
+            borderRadius: BorderRadius.circular(10.0),
             child: Stack(
               children: [
                 ColorFiltered(
